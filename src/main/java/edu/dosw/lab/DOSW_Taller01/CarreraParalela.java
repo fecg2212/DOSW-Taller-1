d7d65667c270ffb98814ef7c21e2eb32fbcdd325
Felipe Calvache <felipe.calvache-g@mail.escuelaing.edu.co>
2025-09-05 12:35:32 -0500

    conflict merge

--- src/main/java/edu/dosw/lab/DOSW_Taller01/CarreraParalela.java
+++ src/main/java/edu/dosw/lab/DOSW_Taller01/CarreraParalela.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class CarreraParalela {
 
+    // Función combinada de máximo, mínimo y cantidad
     public static ResultadoCombinado combinarResultados(List<Integer> numeros) {
         int max = numeros.stream().max(Integer::compare).orElse(Integer.MIN_VALUE);
         int min = numeros.stream().min(Integer::compare).orElse(Integer.MAX_VALUE);
@@ -18,12 +19,18 @@ public class CarreraParalela {
         public String maxMultiplo2; // Carril 1
         public String maxDivisor2;  // Carril 2
 
+        public String cantidadPar;   // Carril 1
+        public String cantidadImpar; // Carril 2
+
         public ResultadoCombinado(int maximo, int minimo, int cantidad) {
             this.maximo = maximo;
             this.minimo = minimo;
             this.cantidad = cantidad;
-            this.maxMultiplo2 = null;
-            this.maxDivisor2 = (2 % maximo == 0) ? "Sí" : "No";
+            this.maxMultiplo2 = null;                                // Carril 1
+            this.maxDivisor2 = (2 % maximo == 0) ? "Sí" : "No";      // Carril 2
+            this.cantidadPar = null;                                  // Carril 1
+            this.cantidadImpar = (cantidad % 2 != 0) ? "Sí" : "No";  // Carril 2
         }
     }
 }
+
