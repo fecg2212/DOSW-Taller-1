231055c2d51b7e8506b154f7023fbe50e34f1224
Felipe Calvache <felipe.calvache-g@mail.escuelaing.edu.co>
2025-09-04 19:43:30 -0500

    Alistando el ambiente

---
+++ src/main/java/edu/dosw/lab/DOSW_Taller01/DoswTaller01Application.java
@@ -0,0 +1,13 @@
+package edu.dosw.lab.DOSW_Taller01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoswTaller01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoswTaller01Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/dosw/lab/DOSW_Taller01/DoswTaller01ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.lab.DOSW_Taller01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoswTaller01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
