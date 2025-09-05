76086f48bdafc74cc1256b4be339c59029463425
Felipe Calvache <felipe.calvache-g@mail.escuelaing.edu.co>
2025-09-05 11:39:23 -0500

    Resolver primer choque: función combinada de máximo, mínimo y cantidad

--- src/main/java/edu/dosw/lab/DOSW_Taller01/CarreraParalela.java
+++ src/main/java/edu/dosw/lab/DOSW_Taller01/CarreraParalela.java
@@ -4,24 +4,27 @@ import java.util.List;
 
 public class CarreraParalela {
 
-    public static ResultadoMinimo obtenerMinimoYCantidad(List<Integer> numeros) {
-        int min = numeros.stream()
-                .min(Integer::compare)
-                .orElse(Integer.MAX_VALUE);
+    // Función combinada de máximo, mínimo y cantidad
+    public static ResultadoCombinado combinarResultados(List<Integer> numeros) {
+        int max = numeros.stream().max(Integer::compare).orElse(Integer.MIN_VALUE);
+        int min = numeros.stream().min(Integer::compare).orElse(Integer.MAX_VALUE);
         int cantidad = numeros.size();
-        return new ResultadoMinimo(min, cantidad);
+        return new ResultadoCombinado(max, min, cantidad);
     }
-
-    public static class ResultadoMinimo {
+    
+    public static class ResultadoCombinado {
+        public int maximo;
         public int minimo;
         public int cantidad;
 
-        public ResultadoMinimo(int minimo, int cantidad) {
+        public ResultadoCombinado(int maximo, int minimo, int cantidad) {
+            this.maximo = maximo;
             this.minimo = minimo;
             this.cantidad = cantidad;
         }
     }
 }
+
 
 
 
