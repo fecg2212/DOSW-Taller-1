3825c9620f4f162442b2d0dfdecc7e61bb0a794c
Felipe Calvache <felipe.calvache-g@mail.escuelaing.edu.co>
2025-09-05 15:04:42 -0500

    Creación clase

---
+++ src/main/java/edu/dosw/lab/DOSW_Taller01/EcoMisterioso.java
@@ -0,0 +1,9 @@
+package edu.dosw.lab.DOSW_Taller01;
+
+public class EcoMisterioso {
+
+	public static String invertirConBuffer(String mensaje) {
+		return new StringBuffer(mensaje).reverse().toString();
+	}
+}
+
