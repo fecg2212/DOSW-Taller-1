cb61979069167bcb920ac1a19b94a48c6049cf1d
Felipe Calvache <felipe.calvache-g@mail.escuelaing.edu.co>
2025-09-05 11:29:51 -0500

    Carril 2: función lambda para calcular número mínimo y cantidad

--- src/main/java/edu/dosw/lab/DOSW_Taller01/CarreraParalela.java
+++ src/main/java/edu/dosw/lab/DOSW_Taller01/CarreraParalela.java
@@ -4,12 +4,24 @@ import java.util.List;
 
 public class CarreraParalela {
 
-    public static int numeroMaximo(List<Integer> numeros) {
-        return numeros.stream()
-                .max(Integer::compare)
-                .orElse(Integer.MIN_VALUE);
+    public static ResultadoMinimo obtenerMinimoYCantidad(List<Integer> numeros) {
+        int min = numeros.stream()
+                .min(Integer::compare)
+                .orElse(Integer.MAX_VALUE);
+        int cantidad = numeros.size();
+        return new ResultadoMinimo(min, cantidad);
     }
+    
+    public static class ResultadoMinimo {
+        public int minimo;
+        public int cantidad;
 
+        public ResultadoMinimo(int minimo, int cantidad) {
+            this.minimo = minimo;
+            this.cantidad = cantidad;
+        }
+    }
 }
+
 
 
